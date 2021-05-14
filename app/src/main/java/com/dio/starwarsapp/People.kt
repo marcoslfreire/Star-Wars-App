ea6c5304939422cb5de668179a884090333e9cae
Marcos Luciano <marcos3dt@gmail.com>
2021-05-14 12:07:51 -0300

    Classe para representa o resultado da api People feita

--- app/src/main/java/com/dio/starwarsapp/People.kt
+++ app/src/main/java/com/dio/starwarsapp/People.kt
@@ -2,17 +2,14 @@ package com.dio.starwarsapp
 
 import com.google.gson.annotations.SerializedName
 
-
-
 data class People (
-
-@SerializedName("name")
-var name: String?,
-@SerializedName("homeworld")
-var planet_origin: String?,
-@SerializedName("gender")
-var gender: String?,
-@SerializedName("birth_year")
-var birth_year: String?
+    @SerializedName("name")
+    var name: String?,
+    @SerializedName("homeworld")
+    var homeworld: String?,
+    @SerializedName("gender")
+    var gender: String?,
+    @SerializedName("birth_year")
+    var birth_year: String?
 
 )
---
+++ app/src/main/java/com/dio/starwarsapp/PeopleResult.kt
@@ -0,0 +1,20 @@
+package com.dio.starwarsapp
+
+import com.google.gson.annotations.SerializedName
+
+class PeopleResult (
+    //https://swapi.dev/api/people/
+    //swapi.dev/api/planets/
+    //swapi.dev/api/films/
+    @SerializedName("count")
+    val count : String,
+    @SerializedName("next")
+    val next : String,
+    @SerializedName("previous")
+    val previous : String,
+    @SerializedName("results")
+    val results: List<People>
+        )
+
+
+
