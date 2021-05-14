a3cc810a20a1ed1ad80c0519d31d3f64f6998ef9
Marcos Luciano <marcos3dt@gmail.com>
2021-05-14 17:20:38 -0300

    funçao criada para mostrar indicador de carregamento progressBar

--- app/src/main/java/com/dio/starwarsapp/MainActivity.kt
+++ app/src/main/java/com/dio/starwarsapp/MainActivity.kt
@@ -2,27 +2,37 @@ package com.dio.starwarsapp
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
 import androidx.recyclerview.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
     private lateinit var recycle_view_list: RecyclerView
+    private lateinit var progressbar_load_indicator : ConstraintLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        recycle_view_list = findViewById(R.id.recycle_view_list)
+        progressbar_load_indicator = findViewById(R.id.progressbar_load_indicator)
+
 
     }
 
-    fun showData(list: List<People>){
-        recycle_view_list.text =""
+    private fun showData(list: List<People>){
+        recycle_view_list.adapter
         list.forEach { people ->
-            recycle_view_list.append("${people.name}-" +
+            recycle_view_list.apply {
+                hasFixedSize()
+                ("${people.name}-" +
                     "${people.birth_year}-${people.gender}-" +
                     "${people.homeworld} \n\n")
         }
+    } }
+    private fun showLoadingIndicator(){
+        progressbar_load_indicator.visibility = View.VISIBLE
     }
-    
 
 }
 
