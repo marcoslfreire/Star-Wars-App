bee5f4b314da7a6d4f48b69b0abb7af544fe9c88
Marcos Luciano <marcos3dt@gmail.com>
2021-05-14 14:31:55 -0300

    class PeopleRepository pronta consuas funcoes

--- app/src/main/java/com/dio/starwarsapp/PeopleRepository.kt
+++ app/src/main/java/com/dio/starwarsapp/PeopleRepository.kt
@@ -5,12 +5,14 @@ import okhttp3.OkHttpClient
 import okhttp3.Request
 
 class PeopleRepository {
-    fun loadData(){
+    fun loadData(): List<People>? {
         val client = OkHttpClient()
         val request = Request.Builder()
             .url("https://swapi.dev/api/people/")
             .build()
         val response = client.newCall(request).execute()
+        val result = parseGsonToResult(response.body()?.string())
+        return result.results
     }
     fun parseGsonToResult(json: String?) =
         Gson().fromJson(json, PeopleResult::class.java)
