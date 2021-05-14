d8c8fe89fe315685e89daed66abc1138765e75ad
Marcos Luciano <marcos3dt@gmail.com>
2021-05-14 14:21:21 -0300

    função para para converter o json feita

--- app/src/main/java/com/dio/starwarsapp/PeopleRepository.kt
+++ app/src/main/java/com/dio/starwarsapp/PeopleRepository.kt
@@ -1,11 +1,18 @@
 package com.dio.starwarsapp
 
+import com.google.gson.Gson
 import okhttp3.OkHttpClient
 import okhttp3.Request
 
 class PeopleRepository {
-    val client = OkHttpClient()
-    val request = Request.Builder()
-        .url("https://swapi.dev/api/people/")
-        .build()
+    fun loadData(){
+        val client = OkHttpClient()
+        val request = Request.Builder()
+            .url("https://swapi.dev/api/people/")
+            .build()
+        val response = client.newCall(request).execute()
+    }
+    fun parseGsonToResult(json: String?) =
+        Gson().fromJson(json, PeopleResult::class.java)
 }
+
